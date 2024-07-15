e1558a6341869c18bc07de14f08bdc6ce21b4ed5
felipesveiga <felipesveiga@gmail.com>
2024-07-15 05:23:48 -0300

    Aula 79

--- projects/frontend/src/App.js
+++ projects/frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React. Oi!
+          Learn React. Run test again
         </a>
       </header>
     </div>
