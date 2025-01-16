947cfaec63da5c3c783bf2ed5f7a377471a02e39
felipesveiga <felipesveiga@gmail.com>
2025-01-16 12:42:13 -0300

    Aula 279

--- projects/complex-gh/deploy.sh
+++ projects/complex-gh/deploy.sh
@@ -5,15 +5,21 @@ ROOT=k8s
 for image in "${images[@]}";
 do 
     image_path="$DOCKER_USERNAME"/multi-"$image"
-    docker build -t "$image_path" -f ./"$image"/Dockerfile ./"$image"
+    docker build -t "$image_path":latest -t "$image_path":"$SHA" -f ./"$image"/Dockerfile ./"$image"
     echo Built image "$image"
-    docker push "$image_path";
-    echo Pushed "$image"
+    docker push "$image_path":latest
+    docker push "$image_path":"$SHA"
+    echo Pushed "$image";
 done
 
 # Now, initializing the cluster's components.
 for dir in $(ls $ROOT);
     do 
         kubectl apply -f $ROOT/$dir;
-        echo Applied "$dir" components
+        echo Applied "$dir" components;
 done
+
+# Now, setting the Deployments' images. 
+for image in "{$images[@]}";
+do 
+    kubectl set image deployments/"$image"-deployment "$image"="$DOCKER_USERNAME"/multi-"$image":"$SHA" # Replace $DOCKER_USERNAME for stephengrider in case of any issue.
